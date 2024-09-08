f235937729bc45835a4f24a368190686d66c298b
SilasFerreira20 <124620806+SilasFerreira19@users.noreply.github.com>
2024-09-08 13:31:36 -0300

    [feat] Primeira atividade POO

---
+++ FazerVsCode/src/RA_2920482411002.java
@@ -0,0 +1,8 @@
+public class RA_2920482411002 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("========= Atividade POO =============");
+        System.out.println("NOME: Silas Ferreira de Moura\nRGM: 2920482411002\nIDADE: 20 anos");
+        System.out.println("GITHUB: @SilasFerreira19\nLINKEDIN: @silas-ferreira-de-moura-b4780b2b6\nCURSO E TURMA: Analise e Desenvolvimento de Sistemas 2º Semestre");
+        System.out.println("=====================================");
+    }
+}
